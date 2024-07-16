e071aca27b5650eecdbc9029bd5c466db81f7487
Gabriel A. Devenyi <gdevenyi@gmail.com>
2024-07-16 15:13:16 -0400

    Update conf.py for ReadTheDocs Changes

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Set canonical URL from the Read the Docs Domain
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = Tru
 
 # -- Project information -----------------------------------------------------
 
