2354f4d2f946f6a4c01984c4039ac68d39615b52
Gabriel A. Devenyi <gdevenyi@gmail.com>
2024-07-16 15:13:37 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
 
 # Tell Jinja2 templates the build is running on Read the Docs
 if os.environ.get("READTHEDOCS", "") == "True":
-    html_context["READTHEDOCS"] = Tru
+    html_context["READTHEDOCS"] = True
 
 # -- Project information -----------------------------------------------------
 
